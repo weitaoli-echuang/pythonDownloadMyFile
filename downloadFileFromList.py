73b9d8467d2582b4684d4aeaeeba48eae791c4bf
weitaoli-echuang <weitaoli.echung@gmail.com>
2016-05-17 20:27:11 +0800

    work version

--- downloadFileFromList.py
+++ downloadFileFromList.py
@@ -1,38 +1,59 @@
 # coding:utf-8
-import urllib
-import urllib2
 import json
 import requests
+import os
+
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 __author__ = 'VDTConstructor'
 
-# url = 'http://www.cninfo.com.cn/cninfo-new/disclosure/sse_latest'
-url = 'http://www.cninfo.com.cn/cninfo-new/announcement/query'
-values = {'stock': '',
-          'searchkey': '2014年年度报告摘要',
-          'plate': '',
-          'category': '',
-          'trade': '',
-          'column': 'sse',
-          'columnTitle': '历史公告查询',
-          'pageNum': '1',
-          'pageSize': '30',
-          'tabName': 'fulltext',
-          'sortName': '',
-          'sortType': '',
-          'limit': '',
-          'showTitle': '',
-          'seDate': '请选择日期'
-          }
 
-r = requests.post(url, data=values)
-# print r.text
+def announcement(url, values):
+	announcement_list = []
+	content = requests.post(url, data=values)
+	content_json = json.loads(content.text)
+	announcement_list.append(content_json['announcements'])
+	while content_json['hasMore'] and len(announcement_list) < 3:
+		values['pageNum'] = str(int(values['pageNum']) + 1)
+		content = requests.post(url, data=values)
+		content_json = json.loads(content.text)
+		announcement_list.append(content_json['announcements'])
 
-the_page_json = json.loads(r.text)
-print the_page_json
+	return announcement_list
 
-print the_page_json['hasMore']
-file_object = open('thefile.txt', 'w')
-file_object.write(r.text)
-file_object.close()
-print 'finish'
+
+if __name__ == '__main__':
+	pdf_download_url = 'http://www.cninfo.com.cn/'
+	page_url = 'http://www.cninfo.com.cn/cninfo-new/announcement/query'
+	page_values = {'stock': '',
+	               'searchkey': '2014年年度报告摘要',
+	               'plate': '',
+	               'category': '',
+	               'trade': '',
+	               'column': 'sse',
+	               'columnTitle': '历史公告查询',
+	               'pageNum': '1',
+	               'pageSize': '30',
+	               'tabName': 'fulltext',
+	               'sortName': '',
+	               'sortType': '',
+	               'limit': '',
+	               'showTitle': '',
+	               'seDate': '请选择日期'
+	               }
+
+	page_content = announcement(page_url, page_values)
+	print len(page_content[0])
+
+	for page in page_content:
+		for item in page:
+			# print item['announcementId'], item['secName'], item['adjunctUrl']
+			download_path = pdf_download_url + item['adjunctUrl']
+			announcement_time = item['adjunctUrl'].split('/')[2]
+			# print download_path
+			command_str = 'wget ' + download_path + ' -O ' + item['secName'] + '_' + announcement_time
+			# print command_str
+			os.system(command_str)
