2e66063f491cd433e2eb09748bd6296587fc60b3
lwt <weitaoli.echung@gmail.com>
2016-05-19 17:29:56 +0800

    change file name

--- downloadFileFromList.py
+++ downloadFileFromList.py
@@ -1,4 +1,4 @@
-# coding:utf-8
+# coding:UTF-8
 import json
 import requests
 import os
@@ -12,48 +12,51 @@ __author__ = 'VDTConstructor'
 
 
 def announcement(url, values):
-	announcement_list = []
-	content = requests.post(url, data=values)
-	content_json = json.loads(content.text)
-	announcement_list.append(content_json['announcements'])
-	while content_json['hasMore'] and len(announcement_list) < 3:
-		values['pageNum'] = str(int(values['pageNum']) + 1)
-		content = requests.post(url, data=values)
-		content_json = json.loads(content.text)
-		announcement_list.append(content_json['announcements'])
+    announcement_list = []
+    content = requests.post(url, data=values)
+    content_json = json.loads(content.text)
+    announcement_list.append(content_json['announcements'])
+    while content_json['hasMore'] and len(announcement_list) < 3:
+        values['pageNum'] = str(int(values['pageNum']) + 1)
+        content = requests.post(url, data=values)
+        content_json = json.loads(content.text)
+        import pdb; pdb.set_trace()  # breakpoint fc1a5200 //
+        announcement_list.append(content_json['announcements'])
 
-	return announcement_list
+    return announcement_list
 
 
 if __name__ == '__main__':
-	pdf_download_url = 'http://www.cninfo.com.cn/'
-	page_url = 'http://www.cninfo.com.cn/cninfo-new/announcement/query'
-	page_values = {'stock': '',
-	               'searchkey': '2014年年度报告摘要',
-	               'plate': '',
-	               'category': '',
-	               'trade': '',
-	               'column': 'sse',
-	               'columnTitle': '历史公告查询',
-	               'pageNum': '1',
-	               'pageSize': '30',
-	               'tabName': 'fulltext',
-	               'sortName': '',
-	               'sortType': '',
-	               'limit': '',
-	               'showTitle': '',
-	               'seDate': '请选择日期'
-	               }
+    pdf_download_url = 'http://www.cninfo.com.cn/'
+    page_url = 'http://www.cninfo.com.cn/cninfo-new/announcement/query'
+    page_values = {'stock': '',
+                   'searchkey': '2014年年度报告摘要',
+                   'plate': '',
+                   'category': '',
+                   'trade': '',
+                   'column': 'sse',
+                   'columnTitle': '历史公告查询',
+                   'pageNum': '1',
+                   'pageSize': '30',
+                   'tabName': 'fulltext',
+                   'sortName': '',
+                   'sortType': '',
+                   'limit': '',
+                   'showTitle': '',
+                   'seDate': '请选择日期'
+                   }
 
-	page_content = announcement(page_url, page_values)
-	print len(page_content[0])
+    page_content = announcement(page_url, page_values)
+    print len(page_content[0])
 
-	for page in page_content:
-		for item in page:
-			# print item['announcementId'], item['secName'], item['adjunctUrl']
-			download_path = pdf_download_url + item['adjunctUrl']
-			announcement_time = item['adjunctUrl'].split('/')[2]
-			# print download_path
-			command_str = 'wget ' + download_path + ' -O ' + item['secName'] + '_' + announcement_time
-			# print command_str
-			os.system(command_str)
+    for page in page_content:
+        for item in page:
+            # print item['announcementId'], item['secName'], item['adjunctUrl']
+            download_path = pdf_download_url + item['adjunctUrl']
+            announcement_time = item['adjunctUrl'].split('/')[2]
+            # print download_path
+            # command_str = 'wget ' + download_path + ' -O ' + \
+            #     item['secName'] + '_' + announcement_time
+            command_str = 'wget ' + download_path
+            # print command_str
+            os.system(command_str)
