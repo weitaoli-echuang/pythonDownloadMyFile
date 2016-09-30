d8895cb9a6d2d270632c268a61604a0e09670782
lwt <weitaoli.echung@gmail.com>
2016-09-30 14:21:34 +0800

    add encode of train number

--- ticket_left_confirm.py
+++ ticket_left_confirm.py
@@ -20,27 +20,43 @@ sys.setdefaultencoding('utf-8')
 
 __author__ = 'VDTConstructor'
 
-
 if __name__ == '__main__':
-    ticket_url = 'https://kyfw.12306.cn/otn/leftTicket/queryT'
+    # ticket_url = 'https://kyfw.12306.cn/otn/leftTicket/queryT'
 
-    page_values = OrderedDict()
-    page_values['leftTicketDTO.train_date'] = '2016-10-01'
-    page_values['leftTicketDTO.from_station'] = 'JNK'
-    page_values['leftTicketDTO.to_station'] = 'JIK'
-    page_values['purpose_codes'] = 'ADULT'
+    # page_values = OrderedDict()
+    # page_values['leftTicketDTO.train_date'] = '2016-10-01'
+    # page_values['leftTicketDTO.from_station'] = 'JNK'
+    # page_values['leftTicketDTO.to_station'] = 'JIK'
+    # page_values['purpose_codes'] = 'ADULT'
 
-    ticket_queue_string_list = [key + '=' +
-                                page_values[key] for key in page_values]
-    ticket_queue_string = ticket_url + '?' + \
-        ('&').join(ticket_queue_string_list)
+    # ticket_queue_string_list = [key + '=' +
+    #                             page_values[key] for key in page_values]
+    # ticket_queue_string = ticket_url + '?' + \
+    #     ('&').join(ticket_queue_string_list)
 
-    content = requests.get(ticket_queue_string, verify=False)
-    # print content.text
-    type(content.text)
-    f = open('ticket.txt', 'w')
-    f.write(content.text)
+    # content = requests.get(ticket_queue_string, verify=False)
+    # # print content.text
+    # f = open('ticket.txt', 'w')
+    # f.write(content.text)
+    # f.close()
+
+    f = open('ticket.txt', 'r')
+    queue_content = f.read()
     f.close()
 
-    # content_json = json.loads(content.text)
-    # print content_json
+    json_content = json.loads(queue_content)
+    # print json_content
+
+    train = json_content['data'][1]['queryLeftNewDTO']
+    for key in train:
+        print key, ' ', train[key]
+
+    f = open('train.txt', 'w')
+    trian_string = [str(key) + ' ' + str(train[key]) for key in train]
+    f.write(('\n').join(trian_string))
+    f.close()
+    # for train in json_content['data']:
+    #     print train['queryLeftNewDTO']['station_train_code']
+
+# content_json = json.loads(content.text)
+# print content_json
